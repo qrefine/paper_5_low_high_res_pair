79d139325755527b12aad636a46d57e2cffde91c
yanting0928 <1303697177@qq.com>
2019-06-20 20:00:53 +0800

    each chain match info

--- perfect_pair.py
+++ perfect_pair.py
@@ -44,6 +44,7 @@ def get_perfect_pair(model, params, chain_ids=None):
   pdb_id = os.path.basename(params.model_name).strip("pdb")[:4].upper()
   h = model.get_hierarchy()
   results =  []
+  count = 0
   for chain in h.only_model().chains():
     if not chain.is_protein():
       continue
@@ -56,40 +57,53 @@ def get_perfect_pair(model, params, chain_ids=None):
     pdb_ids_to_study = {}
     for hit in blast_summary:
       #hit.show(out=sys.stdout)
+      ali_identity = 100*hit.length/chain.residue_groups_size()
+      if ali_identity < 95:
+        continue
       if hit.identity < params.identity:
         continue
-      pdb_ids_to_study[hit.pdb_id] = hit.chain_id 
+      pdb_ids_to_study[hit.pdb_id] = (hit.chain_id,hit.identity)
     #
     info_list = pdb_info.get_info_list(pdb_ids_to_study.keys())
     info_list.sort(key=lambda tup: tup[1])
     if info_list :
       best_pdb_id = info_list[0][0]
-      best_pdb_chain = pdb_ids_to_study[info_list[0][0]]
+      best_pdb_chain = pdb_ids_to_study[info_list[0][0]][0]
+      identity = pdb_ids_to_study[info_list[0][0]][1]
     #  print "Best pdb:", info_list[0], "chain:", pdb_ids_to_study[info_list[0][0]]
       if info_list[0][1] < params.high_res:
-        result = group_args(
-          low_res_pdb  = pdb_id+'_'+chain.id ,
-          high_res_pdb = best_pdb_id +'_'+ best_pdb_chain)
-        results.append(result) 
-  return results
+        #print "%2s  %7s  %.2f  %.2f"%(chain.id,best_pdb_id +'_'+ best_pdb_chain,info_list[0][1],identity)
+        results.append((chain.id,best_pdb_id +'_'+ best_pdb_chain,info_list[0][1],identity))
+      else:
+        #print "%2s  not match"%chain.id
+        count += 1
+    else:
+      #print "%2s  not match"%chain.id
+      count += 1
+  return results,count
 
 def run(params):
-  work_dir = "/home/pdb/pdb/" 
-  pdb_info = easy_pickle.load(file_name='pdb_dict.pickle')
+  work_dir = "/home/pdb/pdb/"
+  pdb_info = easy_pickle.load(file_name='pdb_info.pkl')
   for key,value in pdb_info.items():
-    if value[0] > params.low_res:
-      file_name = "pdb"+key.lower()+".ent.gz"
-      file_path = os.path.join(work_dir,key.lower()[1:3],file_name)
-      if os.path.isfile(file_path):
+    if value.resolution > params.low_res \
+      and value.have_experimental_data == True and \
+      ("X-RAY DIFFRACTION" in value.data_type or "ELECTRON MICROSCOPY" in value.data_type):
+      model_name = "pdb"+key.lower()+".ent.gz"
+      model_path = os.path.join(work_dir,key.lower()[1:3],model_name)
+      if os.path.isfile(model_path):
         try:
-          params.model_name = file_path
-          model = mmtbx.model.manager(model_input=iotbx.pdb.input(file_path))
-          rs = get_perfect_pair(model,params)
-          if rs:
-            for r in rs:
-              print "%-10s  %-10s"%(r.low_res_pdb,r.high_res_pdb)
+          params.model_name = model_path
+          model = mmtbx.model.manager(model_input=iotbx.pdb.input(model_path))
+          rs,c = get_perfect_pair(model,params)
+          if c==0:
+            print key.upper(),value.resolution
+            for i in rs:
+              print i
+            print "*"*80
         except Exception, e:
-          print "%s: %s"%(key, e)        
+          pass
+          #print "%s: %s"%(key, e)        
 
 def run_one(params):
   if not os.path.isfile(params.model_name):
@@ -98,10 +112,9 @@ def run_one(params):
       model_input=iotbx.pdb.input(source_info=None, lines=data.readlines()))
   else:
     model = mmtbx.model.manager(model_input=iotbx.pdb.input(params.model_name))
-  rs = get_perfect_pair(model, params)
-  if rs:
-    for r in rs:
-      print "%-10s  %-10s"%(r.low_res_pdb,r.high_res_pdb)
+  rs,c = get_perfect_pair(model, params)
+  for i in rs:
+    print i
 
 if __name__ == '__main__':
   args = sys.argv[1:]
