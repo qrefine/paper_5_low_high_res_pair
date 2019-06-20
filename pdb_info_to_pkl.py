b144a426a001d43f3c1044940b3f0c0c1c2c74af
yanting0928 <1303697177@qq.com>
2019-06-20 19:52:49 +0800

    format changed: contain info from group_args to list

--- pdb_info_to_pkl.py
+++ pdb_info_to_pkl.py
@@ -5,29 +5,25 @@ from libtbx import easy_pickle
 import iotbx.pdb
 from libtbx import group_args
 
-
-
-
 def get_experimental_pdb_info(file_name,have_experimental_data=False):
-  prefix = os.path.basename(file_name)[3:7]
+  prefix = os.path.basename(file_name)[3:7].upper()
   pdb_inp = iotbx.pdb.input(file_name = file_name)
   r_free = pdb_inp.get_r_rfree_sigma().r_free
   r_work = pdb_inp.get_r_rfree_sigma().r_work
   resolution = pdb_inp.resolution()
   data_type = pdb_inp.get_experiment_type()
-  pdb_info = group_args(
-    data_type  = data_type,
-    resolution = resolution,
-    r_free     = r_free,
-    r_work     = r_work,
-    have_experimental_data = have_experimental_data)
-  crystal_symmetry = pdb_inp.crystal_symmetry()
-  if crystal_symmetry.space_group():
-    pdb_info.space_group = crystal_symmetry.space_group().type().lookup_symbol()
-  return (prefix,pdb_info)
+  space_group = None
+  try:
+    crystal_symmetry = pdb_inp.crystal_symmetry()
+    if crystal_symmetry.space_group():
+      space_group = crystal_symmetry.space_group().type().lookup_symbol()
+  except iotbx.pdb.records.FormatError:
+    pass
+  return (prefix,resolution,r_work,r_free,\
+    have_experimental_data,data_type,space_group)
 
 if __name__ == '__main__':
-  if 0:
+  if 1:
     rdict = {}
     path = "/home/pdb/pdb/"
     dpath = "/home/pdb/structure_factors/"    
@@ -43,11 +39,12 @@ if __name__ == '__main__':
         have_experimental_data = True
       try:
         tup = get_experimental_pdb_info(f,have_experimental_data)  
-        rdict[tup[0]] = tup[1]
+        rdict[tup[0]] = tup[1:]
       except Exception,e:
         print pdb_code,"Failed",str(e)
-    easy_pickle.dump(file_name='pdb_dict.pkl', obj=rdict)
+    easy_pickle.dump(file_name='pdb_info.pkl', obj=rdict)
   else:
-    info=get_experimental_pdb_info("/home/pdb/pdb/us/pdb1us0.ent.gz")
+#    info=get_experimental_pdb_info("/home/pdb/pdb/du/pdb6du8.ent.gz")
+    info=get_experimental_pdb_info("/home/pdb/pdb/ny/pdb4ny6.ent.gz")
     print info
    # get_experimental_pdb_info("/home/pdb/pdb/us/pdb1us0.ent.gz")
